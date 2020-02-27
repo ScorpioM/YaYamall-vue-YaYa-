39ad0f06561c97985cf70d24af2ef9cc42df4e83
imperfectX <253444444@qq.com>
2020-02-27 17:03:53 +0800

    修改rem报错问题

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./js/cookie";
-// import './config/rem';
+import './config/rem';
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
